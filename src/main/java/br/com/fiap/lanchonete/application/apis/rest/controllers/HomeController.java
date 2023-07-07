e4336ecbc28cba5c2ca4f73074616b900f6a1eae
Jonilson Sousa <jonilsonds9@gmail.com>
2023-07-07 20:16:03 -0300

    Adicionando controller Home para um direcionamento do usuário

---
+++ src/main/java/br/com/fiap/lanchonete/application/apis/rest/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package br.com.fiap.lanchonete.application.apis.rest.controllers;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        String content = """
+                <!DOCTYPE html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>API Lanchonete</title>
+                </head>
+                <body>
+                    <h1>Bem-vindo a API Lanchonete!</h1><br>
+                    <p>
+                        Veja a documentação em <a href="http://localhost:8081/api/docs">http://localhost:8081/api/docs</a>
+                    </p>
+                </body>
+                </html>
+                """;
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.TEXT_HTML);
+
+        return new ResponseEntity<>(content, responseHeaders, HttpStatus.OK);
+    }
+}
